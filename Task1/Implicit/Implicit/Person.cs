11b39091d64530bbf7350db15ffe1b2c2b5f25ba
ibrahim <ibrahimea@code.edu.az>
2021-10-17 20:02:01 +0400

    Implicit

---
+++ Task1/Implicit/Implicit/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implicit
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name,string surname,int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        
+        public static bool operator > (Person person1,Person person2)
+        {
+            return person1.Age > person2.Age;
+
+        }
+        public static bool operator<(Person person1,Person person2)
+        {
+            return person1.Age < person2.Age;
+
+        }
+    }
+}
---
+++ Task1/Implicit/Implicit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Implicit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //Kelvin kelvin = new Kelvin(50);
+            //Celcius celcius = kelvin;
+            //Console.WriteLine(celcius.Degree);
+            #endregion
+
+            Person person1= new Person("Ibrahim","Aliyev",27);
+            Person person2 = new Person("Samir", "Aliyev", 23);
+            Console.WriteLine(person1>person2);
+
+
+
+        }
+    }
+
+    #region
+    //class Kelvin
+    //{
+    //    public double Degree { get; set; }
+    //    public Kelvin(double a)
+    //    {
+    //        Degree = a;
+    //    }
+    //    public static implicit operator Celcius(Kelvin kelvin)
+    //    {
+    //        return new Celcius(kelvin.Degree - 273);
+
+    //    }
+
+    //}
+
+
+    //class Celcius
+    //{
+    //    public double Degree { get; set; }
+    //    public Celcius(double b)
+    //    {
+    //        Degree = b;
+    //    }
+    //}
+    #endregion 
+}
+
